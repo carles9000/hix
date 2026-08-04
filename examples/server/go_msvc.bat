cdf549e540ec22853e595e8f671013f6d11a040c
Carles Aubia <carles9000@gmail.com>
2026-08-04 08:42:54 +0200

    update examples/server

--- examples/server/go_msvc.bat
+++ examples/server/go_msvc.bat
@@ -2,13 +2,12 @@
 
 if exist "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
-if exist hix_app.exe del hix_app.exe
+if exist hix.exe del hix.exe
 
 
-if not exist "hix_app.res" (	
-	rc -r hix_app.rc
+if not exist "hix.res" (	
+	rc -r hix.rc
 )
-
 
 set hix=c:\hix.project\hix.pro
 set hbdir=c:\harbour
@@ -16,18 +15,16 @@ set include=%include%;%hbdir%\include;%hix%\src\include
 set lib=%lib%;%hbdir%\lib;%hix%
 set path=%path%;c:\windows\system32;c:\windows;%hbdir%;%hbdir%\bin;%hix%
 
-rem set DEBUG_FLAG=-b -DDEBUG
+hbmk2 hix.hbp -comp=msvc64 
 
-hbmk2 hix_app.hbp -comp=msvc64 
-rem pause
 if errorlevel 1 goto compileerror
 
-del hix_app.exp
-del hix_app.lib
-del hix_app.res
+if exist hix.exp del hix.exp 
+if exist hix.lib del hix.lib
+if exist hix.res del hix.res
 
 @cls
-hix_app.exe 
+hix.exe 
 
 goto exit 
 
